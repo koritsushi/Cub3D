229e18617e0dfe9d5e9d1d8635ae8e6eef6e6185
mliyuan <lyuan.2581@gmail.com>
2025-09-26 18:46:52 +0800

    fix(parse_texture): add check_ext() function for checking valid .xpm file and not an directory

--- srcs/parser_texture.c
+++ srcs/parser_texture.c
@@ -6,32 +6,31 @@
 /*   By: mliyuan <mliyuan@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/26 16:41:04 by mliyuan           #+#    #+#             */
-/*   Updated: 2025/09/26 12:32:07 by mliyuan          ###   ########.fr       */
+/*   Updated: 2025/09/26 14:04:21 by mliyuan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cube3d.h"
 
-int	parse_texture(t_cub *data, char *pathname, int type)
+int	parse_texture(t_cub *data, char *path, int type)
 {
-	char	**content;
+	char	**pathname;
 	int		width;
 	int		height;
 	int		fd;
 
-	if (check_ext(&fd, pathname, ".xpm") == 0)
+	pathname = ft_split(path, ' ');
+	if (pathname == NULL || pathname[1] == NULL)
 		return (0);
-	close(fd);
-	content = ft_split(pathname, ' ');
-	if (content == NULL || content[1] == NULL)
+	if (check_ext(&fd, pathname[1], ".xpm") == 0)
 		return (0);
 	data->texture[type].img = mlx_xpm_file_to_image\
-(data->exec.mlx, content[1], &width, &height);
+(data->exec.mlx, pathname[1], &width, &height);
 	if (data->texture[type].img == NULL)
-		return (ft_free_arr((void **) content), 1);
+		return (ft_free_arr((void **) pathname), 1);
 	data->texture[type].address = mlx_get_data_addr\
 (data->texture[type].img, &data->texture[type].bpp, \
 &data->texture[type].line_length, &data->texture[type].endian);
-	ft_free_arr((void **) content);
+	ft_free_arr((void **) pathname);
 	return (1);
 }
